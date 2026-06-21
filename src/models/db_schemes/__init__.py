a0d83374a9c76d666858011857beceef3f1e4008
Abdelrahman Ahmed <ahm_mshmsh2004@yahoo.com>
2026-06-22 02:58:23 +0300

    deleted not needed tools

--- src/models/db_schemes/__init__.py
+++ src/models/db_schemes/__init__.py
@@ -1,3 +1,2 @@
-from .project import Project
 from .data_chunk import DataChunk
 from .crm_ticket import CRMTicket
--- src/models/enums/DataBaseEnum.py
+++ src/models/enums/DataBaseEnum.py
@@ -2,10 +2,5 @@ from enum import Enum
 
 class DataBaseEnum(Enum):
 
-    PROJECT_COLLECTION_NAME = "projects"
     CHUNKS_COLLECTION_NAME = "chunks"
-    ASSET_COLLECTION_NAME = "assets"
-    USERS_COLLECTION_NAME = "users"
-    CHATS_COLLECTION_NAME = "chats"
-    ADMINS_COLLECTION_NAME = "admins"
     CRM_TICKETS_COLLECTION_NAME = "crm_ticket"
